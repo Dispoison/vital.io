65919e6167c83f51936a4883c5b4a3f389bd816e
Vitalii Maliarenko <maliarenko.vitaliy@gmail.com>
2021-04-04 18:02:00 +0300

    Provide basic game functionality

---
+++ map.py
@@ -0,0 +1,10 @@
+from config import *
+
+
+class Map:
+    def __init__(self, food_obj_list, background_color=(0, 0, 0), grid_color=(20, 20, 20)):
+        self.rows = ROWS
+        self.cols = COLS
+        self.food_obj_list = food_obj_list
+        self.background_color = background_color
+        self.grid_color = grid_color
